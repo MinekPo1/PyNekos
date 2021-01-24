5228205ef72fd3c73c26e917bdcb9cdd3be7cee0
ChoiYun <perrique.henrique@gmail.com>
2021-01-24 12:06:05 -0300

    Update examples

--- examples/search_user.py
+++ examples/search_user.py
@@ -5,11 +5,14 @@ File that exemplify the use of the client to get a json with informations using 
 from PyNekos.nekosapi import Neko
 
 nyan = Neko()
-users = nyan.search_user(limit=2)
+search_kw = {
+    'limit': 2
+}
+users = nyan.search_user(**search_kw)
 print(users)
 
 
 """
 Return:
-{'users': [{'id': 'BkCBy21se', 'username': 'brussell', 'createdAt': '2017-03-10T04:44:53.870Z', 'favoritesReceived': 1354, 'likesReceived': 2387, 'uploads': 637, 'roles': ['admin']}, {'id': 'rybWrj90g', 'username': 'Shumatsu', 'createdAt': '2017-04-23T22:04:41.492Z', 'favoritesReceived': 152, 'likesReceived': 278, 'uploads': 71, 'roles': []}]}
+{'users': [{'id': 'BkCBy21se', 'username': 'brussell', 'createdAt': '2017-03-10T04:44:53.870Z', 'favoritesReceived': 1475, 'likesReceived': 2586, 'uploads': 637, 'roles': ['admin']}, {'id': 'rybWrj90g', 'username': 'Shumatsu', 'createdAt': '2017-04-23T22:04:41.492Z', 'favoritesReceived': 155, 'likesReceived': 282, 'uploads': 71, 'roles': []}]}
 """
--- examples/upload_image.py
+++ examples/upload_image.py
@@ -7,15 +7,33 @@ from PyNekos.nekosapi import Neko
 nyan = Neko(token='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 # Danbooru post
-danbooru = nyan.upload_image(image='2613483', upload_type='danbooru', nsfw=False)
+dan_kw = {
+    'image': '2613483',
+    'upload_type': 'danbooru',
+    'nsfw': False
+}
+danbooru = nyan.upload_image(**dan_kw)
 print(danbooru)
 
 # Local post
-local = nyan.upload_image(image='neko.jpg', image_path='/home/user/path/to/image/neko.jpg', upload_type='local', tags=['student'], nsfw=False)
+local_kw = {
+    'image': 'neko.jpg',
+    'image_path': '/home/user/path/to/image/neko.jpg',
+    'upload_type': 'local',
+    'tags': ['student'],
+    'nsfw': False
+}
+local = nyan.upload_image(**local_kw)
 print(local)
 
 # Url post
-url = nyan.upload_image(image='https://ih1.redbubble.net/image.608339956.2125/flat,750x,075,f-pad,750x1000,f8f8f8.jpg', upload_type='url', tags=['animal ear', 'test'], nsfw=False)
+url_kw = {
+    'image': 'https://ih1.redbubble.net/image.608339956.2125/flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+    'upload_type': 'url',
+    'tags': ['animal ear', 'test'],
+    'nsfw': False
+}
+url = nyan.upload_image(**url_kw)
 print(url)
 
 
